4fc4088b065d37a66311e8b1e531b00c309793bb
Tomas Podesta <tomasagustinpodesta@gmail.com>
2021-12-21 21:02:53 -0300

    Clase Valores y PuestoCobro
    Valores terminada, por testear y PuestoCobro no terminada aun.

--- I2T_Proyect_Alone/src/main/java/Modelo/PuestoCobro.java
+++ I2T_Proyect_Alone/src/main/java/Modelo/PuestoCobro.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
+
+import java.util.*;
 
 /**
  *
@@ -10,5 +8,125 @@ package Modelo;
  * Almacena informacion de un puesto
  */
 public class PuestoCobro {
+    //Variables
+    Supermercado supermercado;
+    Cajero cajero;
+    Puesto puesto;
+    Sucursal sucursal;
+    HashMap<String, HashMap<Double, Integer>> valoresDisponibles;
     
+    //Metodos get
+    
+    /**
+     * El metodo devuelve el supermercado responsable del puesto
+     * @param No
+     * @return Supermercado que maneja el puesto
+     */
+    public Supermercado getSupermercado(){
+        return supermercado;
+    }
+    
+    /**
+     * El metodo devuelve el cajero que esta trabajando en el puesto que se esta utilizando
+     * @param No
+     * @return Cajero que esta trabajando en el puesto actual
+     */
+    public Cajero getCajero(){
+        return cajero;
+    }
+    
+    /**
+     * El metodo devuelve el puesto que se esta utilizando
+     * @param No
+     * @return Puesto que se esta utilizando actualmente en este Objeto
+     */
+    public Puesto getPuesto(){
+        return puesto;
+    }
+    
+    /**
+     * El metodo devuelve la Sucursal donde se encuentra el puesto
+     * @param No
+     * @return Sucursal donde se encuentra el puesto utilizado en este momento
+     */
+    public Sucursal getSucursal(){
+        return sucursal;
+    }
+    
+    /**
+     * El metodo devuelve los valores posibles junto con sus cantidades disponibles en el puesto
+     * @param No
+     * @return Valores posibles junto con sus cantidades disponibles en el puesto
+     */
+    public HashMap<String, HashMap<Double, Integer>> getValoresDisponibles(){
+        return valoresDisponibles;
+    }
+    
+    //Metodos set
+    
+    /**
+     * El metodo cambia el supermercado responsable del puesto
+     * @param Supermercado responsable del puesto
+     * @return No
+     */
+    public void setSupermercado(Supermercado superInput) {
+        this.supermercado = superInput;
+    }
+    
+    /**
+     * El metodo cambia el cajero que trabaja en el puesto
+     * @param cajero que esta trabajando en el puesto
+     * @return No
+     */
+    public void setCajero(Cajero cajeroInput) {
+        this.cajero = cajeroInput;
+    }
+    
+    /**
+     * El metodo cambia el puesto activo
+     * @param puesto activo
+     * @return No
+     */
+    public void setPuesto(Puesto puestoInput) {
+        this.puesto = puestoInput;
+    }
+    
+    /**
+     * El metodo cambia la sucursal del puesto
+     * @param sucursal donde se encuentra el puesto
+     * @return No
+     */
+    public void setSucursal(Sucursal sucursalInput) {
+        this.sucursal = sucursalInput;
+    }
+    
+    
+    /**
+     * Completar metodo para establecer los valores disponibles (Necesario?)
+     */
+    public void setValoresDisponibles(HashMap<String, HashMap<Double, Integer>> valoresInput) {
+        this.valoresDisponibles = valoresInput;
+    }
+    
+    //Resto de metodos 
+    /**
+     * Este metodo sirve para registrar un ingreso de dinero en el sistema
+     * @param tipoValor: Si es billete o moneda
+     * @param denominacion: tamanio del billete o moneda
+     * @param cantidadDineroIngresado: cantidad del dinero ingresado
+     * @return true (operacion exitosa) o false (operacion fallida)
+     */
+    public Boolean ingresarDinero(String tipoValor, Double denominacion, Integer cantidadDineroIngresado){
+        if(tipoValor != "Billete" || tipoValor != "Moneda"){
+            return false;
+        }
+        
+        if(!valoresDisponibles.containsKey(denominacion)){
+            return false;
+        }
+        
+        HashMap<Double, Integer> mapa = valoresDisponibles.get(tipoValor);
+        mapa.put(denominacion, mapa.get(denominacion) + cantidadDineroIngresado);
+        return true;
+    }
 }
--- I2T_Proyect_Alone/src/main/java/Modelo/Valores.java
+++ I2T_Proyect_Alone/src/main/java/Modelo/Valores.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
+
+//Imports
+import java.util.*;
 
 /**
  *
@@ -10,5 +9,39 @@ package Modelo;
  * Contiene los valores de los billetes y monedas
  */
 public class Valores {
+    
+    HashMap<String, HashMap<Double, String>> valoresDisponibles;
+    /**
+     * Muestra en pantalla los posibles valores que tiene la moneda pesos argentinos.
+     * @param No
+     * @throws valores disponibles de la moneda pesos
+     */
+    public HashMap<String, HashMap<Double, String>> getValoresDisponiblesMapa2D(){
+        return valoresDisponibles; //testear
+    }
+    
+    /**
+     * Este metodo devuelve los valores disponibles de billetes o monedas, segun se ingrese
+     * @param La palabra "Billete" o "Moneda"
+     * @throws Todos los valores disponibles de los billetes o monedas en pesos argentinos
+     */
+    public HashMap<Double, String> getSegDimValoresDisponiblesMapa2D(String billeOMoneda){
+        if(billeOMoneda == "Billete" || billeOMoneda == "Moneda"){
+            return valoresDisponibles.get(billeOMoneda);
+        } else {
+            return null;
+        } //Testear
+    }
+    
+    /**
+     * Metodo principal de la clase.
+     * @param La palabra "Billete" o "Moneda" y una denominacion.
+     * @throws La representacion escrita de la denominacion ingresada.
+     */
+    public String getUnaDescripcion(String inputStr, Double inputDouble){
+        String result = null;
+        result = getValoresDisponiblesMapa2D().get(inputStr).get(inputDouble);
+        return result; //testear
+    }
     
 }
